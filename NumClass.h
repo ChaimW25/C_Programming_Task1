cf694184682c33f25e5c71c15004ac5a8c334d28
ChaimW25 <74601548+ChaimW25@users.noreply.github.com>
2021-11-08 13:11:40 +0200

    Update NumClass.h

--- NumClass.h
+++ NumClass.h
@@ -1,36 +1,6 @@
-#include "NumClass.h"
-#include <stdio.h>
-#include <math.h>
-
-int isPrime(int a){
-
- for (size_t i = 2; i <= a/2; i++)
- {
-    if(a%i==0){
-        return FALSE;
-    }
- }
- return TRUE;
-    
-}
-int factorial(int s){
-    int mult=1;
-    for (size_t i = 1; i <= s; i++)
-    {
-        mult*=i;
-    }
-    return mult;
-}
-int isStrong(int a){
-int temp=a;
-int sum=0;
-while (temp>0)
-{
-    sum+=factorial(temp%10);
-    temp/=10;
-}
-if(sum==a){
-    return TRUE;
-}
-return FALSE;
-}
+int isArmstrong(int n);
+int isPalindrome(int n);
+int isPrime(int n);
+int isStrong(int n);
+#define TRUE 1;
+#define FALSE 0;
